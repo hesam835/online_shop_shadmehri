ff87e6185138ef207e5c4cf2faee3aa0e8da8646
hesam835 <hesamshadmehri@gmail.com>
2024-01-25 04:30:56 -0500

    correct models for sof delete and core

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    STAFF_CHOICES = [
+    ('Customer','Customer'),
+    ('Admin','Admin'),
+    ('Supervisor','Supervisor')
+    ]
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField(max_length=200)
+    image = models.ImageField(null=True)
+    role = models.CharField(max_length = 30 , choices=STAFF_CHOICES)
+
+class adress(models.Model):
+    province = models.CharField(max_length=3)
+    city = models.CharField(max_length = 50)
+    detailed_address = models.TextField()
+    postal_code = models.BigIntegerField()
+
+
--- order/models.py
+++ order/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from accounts.models import User
+from product.models import Discount_Code,Product
 
 # Create your models here.
+
+
+class Order(models.Model):
+    total_price = models.DecimalField(max_digits = 10 , decimal_places = 2)
+    is_paid = models.BooleanField(Default = False)
+    user_id = models.ForeignKey(User , on_deleted = models.CASCADE)
+    discount_code_id = models.ForeignKey(Discount_Code,on_deleted = models.CASCADE)
+    
+    
+class Orde_Item(models.Model):
+    quantity = models.IntegerField(max_length = 2)
+    order_id = models.ForeignKey(Order , on_deleted = models.CASCADE)
+    product_id = models.ForeignKey(Product , on_deleted = models.CASCADE)
+
+
+class Transaction(models.Model):
+    total_price = models.DecimalField(max_digits = 10 , decimal_places = 2)
+    transaction_type = models.CharField(max_length = 50)
+    user_id = models.ForeignKey(User , on_deleted = models.CASCADE)
+    order_id = models.ForeignKey(Order , on_deleted = models.CASCADE)
+    discount_code_id = models.ForeignKey(Discount_Code,on_deleted = models.CASCADE)
+    
+    
+    
