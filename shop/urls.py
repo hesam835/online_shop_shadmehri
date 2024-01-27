d7b092bc0ef43e3358e0ec46d61b70dfacfe0875
hesam835 <hesamshadmehri@gmail.com>
2024-01-27 14:38:34 -0500

    create login template-set views and urls for login

--- shop/urls.py
+++ shop/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from product import urls as product_urls
-
+from accounts import urls as accounts_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('product/',include(product_urls)),
+    path('accounts/',include(accounts_urls)),
 ]
